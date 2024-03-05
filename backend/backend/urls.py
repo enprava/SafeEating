03defd8d0d5ab2bfb931b94339baee99ff31a968
enprava <epradavazquez@gmail.com>
2024-03-05 21:14:46 +0100

    Removed api/ from all urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -35,18 +35,18 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("api/admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path(
-        "api/swagger<format>/",
+        "swagger<format>/",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
     path(
-        "api/",
+        "",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
     path(
-        "api/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+        "redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
 ]
