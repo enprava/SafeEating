0c6fe0e1f3ef09b3c5e07157872e45be3eba4f5a
enprava <epradavazquez@gmail.com>
2024-04-25 21:07:39 +0200

    Quickfixes

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -8,7 +8,7 @@ function Footer({ className = ""}: args) {
     return (
         <div className={"sticky bottom-0 w-full bg-white flex justify-between border-t border-solid border-border " + className}>
             <a href="/"><HomeIcon className="h-8 ml-2 mb-px" /></a>
-            <a href="/search"><MagnifyingGlassIcon className="h-8 mb-px" /></a>
+            <a href="/establishments"><MagnifyingGlassIcon className="h-8 mb-px" /></a>
             <HeartIcon className="h-8" />
             <a href="/settings"><UserIcon className="h-8 mr-2" /></a>
         </div>
