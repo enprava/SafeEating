86ad1f08b634e237af23d02c7274bc1505be2529
enprava <epradavazquez@gmail.com>
2024-05-03 20:20:36 +0200

    Added loadMore to establishments pages and quickfix the infinite loop

--- frontend/src/pages/establishments/index.tsx
+++ frontend/src/pages/establishments/index.tsx
@@ -6,31 +6,35 @@ import URL_API from "@/utils/url-api";
 import Loading from "@/components/loading";
 import Establishment from "@/components/establishment";
 import MapComponent from "@/components/maps/establishment";
+import LoadMore from "@/components/load-more";
 
 export default function () {
     const establishmentUrl: string = "/establishment/";
     const mapUrl: string = "/establishment/mapa/";
-    const [establishmentData, setEstablishmentData]: any | {} = useState(null);
+    const [establishmentData, setEstablishmentData]: any= useState([]);
     const [mapData, setMapData]: any | {} = useState(null)
     const [activeTab, setActiveTad]: any | {} = useState(0);
     const location = sessionStorage.getItem("location");
     const radius = 2000;
-    const loading = <Loading className="justify-center items-center flex" style={{height: window.innerHeight -161}}/>;
+    const [lastResponse, _setLastResponse]: any = useState(null);
+    const [establishmentFetched, setEstablishmentFetched] = useState(false);
+    const loading = <Loading className="justify-center items-center flex" style={{ height: window.innerHeight - 161 }} />;
+
 
     function getMapData() {
-        const lon: any = location?.split(',')[0] ? location?.split(',')[0]: "-5.987375667032342";
-        const lat: any = location?.split(',')[1] ? location?.split(',')[1]: "37.3930443446";
+        const lon: any = location?.split(',')[0] ? location?.split(',')[0] : "-5.987375667032342";
+        const lat: any = location?.split(',')[1] ? location?.split(',')[1] : "37.3930443446";
         fetch(`${URL_API}${mapUrl}${lon},${lat},${radius}/`)
             .then((response) => response.json())
             .then((data) => setMapData(data));
 
     }
 
-    function getEstablishmentData() {
-        fetch(URL_API + establishmentUrl)
+    function getEstablishmentData(url:string) {
+        fetch(url)
             .then((response) => response.json())
             .then(
-                (data) => setEstablishmentData(data)
+                (data) => setLastResponse(data)
             );
     };
 
@@ -41,20 +45,33 @@ export default function () {
         }
         return <MapComponent data={mapData} />
     }
+    function setLastResponse(response: any) {
+        _setLastResponse(response);
+        setEstablishmentData([...establishmentData, ...response.results])
+        setEstablishmentFetched(true);
+    }
 
+    function getMoreData() {
+        getEstablishmentData(lastResponse.next);
+    }
     function showEstablishmentData() {
-        if (!establishmentData) {
-            getEstablishmentData();
+        if (establishmentData.length == 0) {
+            if (!establishmentFetched) getEstablishmentData(URL_API + establishmentUrl);
             return loading;
         }
 
         const establishments: any = [];
 
-        establishmentData.results.forEach((establishment: any) => establishments.push(
+        establishmentData.forEach((establishment: any) => establishments.push(
             <Establishment id={establishment.id} name={establishment.name} address={establishment.address} website={establishment.website} adaptations={establishment.adaptations} images={establishment.images} location={establishment.location} stars={establishment.stars} />
         ));
-        return establishments;
-            
+        return (
+            <>
+            {establishments}
+            <LoadMore loadMore={!(lastResponse && lastResponse.next)} getMoreData={getMoreData} />
+            </>
+        );
+
     }
     return (
         <>
