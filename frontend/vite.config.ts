7309aa05e478e2a025f98fccd88e5428e6874241
enprava <epradavazquez@gmail.com>
2024-05-06 22:06:07 +0200

    Vite PWA added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   test: {
     coverage: {
       provider: 'v8'
