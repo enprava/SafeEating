8d0953978bbf4a2fe40bd2e957c441ae54a95c0b
enprava <epradavazquez@gmail.com>
2024-05-15 20:47:02 +0200

    Quickfix establishment view

--- frontend/src/pages/establishments/index.tsx
+++ frontend/src/pages/establishments/index.tsx
@@ -64,10 +64,11 @@ export default function EstablishmentList() {
     }
     function toggleChecked(adaptationId: number) {
         checked.has(adaptationId) ? checked.delete(adaptationId) : checked.add(adaptationId);
+        console.log(searchInput)
         if (activeTab == 0)
             getEstablishmentData(`${URL_API}${establishmentUrl}?adaptations=${Array.from(checked).join(",")}&search=${searchInput}`);
         else
-            getMapData(`${URL_API}${mapUrl}${lon},${lat},${radius}/?adaptations=${Array.from(checked).join(",")}&search=${searchInput}1`)
+            getMapData(`${URL_API}${mapUrl}${lon},${lat},${radius}/?adaptations=${Array.from(checked).join(",")}&search=${searchInput}`)
         establishmentData.length = 0;
     }
     function getMoreData() {
@@ -107,11 +108,11 @@ export default function EstablishmentList() {
         setShowAdaptations(!showAdaptations);
     }
     function setSearchInput(event: any){
-        _setSearchInput(event.target.value);
         if (activeTab == 0)
             getEstablishmentData(`${URL_API}${establishmentUrl}?adaptations=${Array.from(checked).join(",")}&search=${event.target.value}`);
         else
             getMapData(`${URL_API}${mapUrl}${lon},${lat},${radius}/?adaptations=${Array.from(checked).join(",")}&search=${event.target.value}`)
+        _setSearchInput(event.target.value);
         establishmentData.length = 0;
     }
     return (
